c3f41f1d11f5379b45338829b8da7135782ade11
erwin maruli <erwinmaruli@live.com>
2020-01-19 10:44:54 +0800

    add OTA capability

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 #include <IotWebConf.h>
 #include <FastLED.h>
 #include <sprite.h>
+#include <matrix.h>
 
 #define LED_PIN  14
-#define BRIGHTNESS 35
+#define BRIGHTNESS 3
 #define CHIPSET WS2812B
 #define COLOR_ORDER GRB
 
@@ -20,6 +21,11 @@ const bool    kMatrixSerpentineLayout = true;
 CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
 CRGB leds[ NUM_LEDS ];
 #define LAST_VISIBLE_LED 255
+
+const int led = 2;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+int ledState = LOW;
 
 DNSServer dnsServer;
 WebServer server(80);
@@ -46,45 +52,14 @@ uint8_t XY (uint8_t x, uint8_t y) {
     return (LAST_VISIBLE_LED + 1);
   }
 
-  const uint8_t XYTable[] = {
-   240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255,
-   224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239,
-   208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
-   192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207,
-   176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
-   160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
-   144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
-   128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143,
-   112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-    96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111,
-    80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
-    64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,
-    48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
-    32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
-    16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
-     0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15
-  };
-
   uint8_t i = (y * kMatrixWidth) + x;
   uint8_t j = XYTable[i];
   return j;
 }
 
-void DrawOneFrame( byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
-  byte lineStartHue = startHue8;
-  for( byte y = 0; y < kMatrixHeight; y++) {
-    lineStartHue += yHueDelta8;
-    byte pixelHue = lineStartHue;      
-    for( byte x = 0; x < kMatrixWidth; x++) {
-      pixelHue += xHueDelta8;
-      leds[ XY(x, y)]  = CHSV( pixelHue, 255, 255);
-    }
-  }
-}
-
 void setup()
 {
+  pinMode(led, OUTPUT);
   Serial.begin(115200);
   Serial.println();
   Serial.println("Starting up...");
@@ -95,15 +70,41 @@ void setup()
   server.on("/config", []{ iotWebConf.handleConfig(); });
   server.onNotFound([](){ iotWebConf.handleNotFound(); });
 
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
   FastLED.setBrightness( BRIGHTNESS );
-  for( byte y = 0; y < kMatrixHeight; y++) {
-    for( byte x = 0; x < kMatrixWidth; x++) {
+  for (byte y = 0; y < kMatrixHeight; y++) {
+    for (byte x = 0; x < kMatrixWidth; x++) {
       if (DigDug01[ kMatrixWidth * y + x ] != 0x000000) {
         leds[ XY(x, y) ]  = pgm_read_dword(&(DigDug01[ kMatrixWidth * y + x ]));
       }
       else {
-        leds[ XY(x, y) ]  = CRGB::Yellow;
+        //leds[ XY(x, y) ]  = CRGB::Yellow;
       }
     }
   }
@@ -115,16 +116,14 @@ void setup()
 void loop()
 {
   iotWebConf.doLoop();
-  /*
-  uint32_t ms = millis();
-  int32_t yHueDelta32 = ((int32_t)cos16( ms * (27/1) ) * (350 / kMatrixWidth));
-  int32_t xHueDelta32 = ((int32_t)cos16( ms * (39/1) ) * (310 / kMatrixHeight));
-  DrawOneFrame( ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
-  if( ms < 5000 ) {
-    FastLED.setBrightness( scale8( BRIGHTNESS, (ms * 256) / 5000));
-  } else {
-    FastLED.setBrightness(BRIGHTNESS);
-  }
-  */
 
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    ledState = not(ledState);
+
+    digitalWrite(led, ledState);
+  }
 }
